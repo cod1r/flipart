9d92429ec17f670c1500d5ae14d6f50929abe4b2
cod1r <jasonho353@gmail.com>
2024-03-16 10:11:05 -0500

    cooking

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
-
+function partition(grid: string[][]): string[][] {
+  for (let r = 0; r < 8; ++r) {
+    for (let c = 0; c < 8; ++c) {}
+  }
+}
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [grid, setGrid] = useState<string[][]>(
+    new Array(8).fill(new Array(8).fill(' '))
+  )
+  useEffect(() => {
+    setGrid(partition(grid))
+  }, [])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div
+      style={{
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div className="container"></div>
+    </div>
   )
 }
 
